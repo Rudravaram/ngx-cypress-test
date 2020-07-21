a57df724083c5878f0d30a80d7604b85ba8b1093
Sri Rudravaram <Sridhar.Chakravarthy@gmail.com>
2020-07-21 16:55:07 +0100

    update

---
+++ cypress/integration/firsttest.spec.js
@@ -0,0 +1,19 @@
+describe ( 'My first test suite', () => {
+
+    it('first test', () => {
+        
+    })
+
+    it('second test', () => {
+        
+    })
+
+    it('third test', () => {
+        
+    })
+
+    it('forth test', () => {
+        
+    })
+
+} )
