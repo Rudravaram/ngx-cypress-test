21d8e3ff412e48e02f284bcd4de49e4482eb760d
Sri Rudravaram <Sridhar.Chakravarthy@gmail.com>
2020-08-04 20:18:32 +0100

    update

--- cypress/integration/firsttest.spec.js
+++ cypress/integration/firsttest.spec.js
@@ -1,58 +1,61 @@
-describe ( 'My first test suite', () => {
+// describe ( 'My first test suite', () => {
 
-    it('first test', () => {
+//         describe ('our first test under the test',()=>{
+
+//             beforeEach('code for repetative action', () => {
+                
+//                 //repetative code
+
+//             })
+
+//             it('internal first test',() => {
+
+//             })
+//         })
+
+//     it('first test', () => {
         
-    })
+//     })
 
-    it('second test', () => {
+//     it('second test', () => {
         
-    })
+//     })
 
-    it('third test', () => {
+//     it('third test', () => {
         
-    })
+//     })
 
-    it('forth test', () => {
+//     it('forth test', () => {
         
-    })
+//     })
 
-} )
+// } )
 
-describe ( 'My Second test suite', () => {
+// describe ( 'My Second test suite', () => {
 
-    it('first test', () => {
+//     it('first test', () => {
         
-    })
+//     })
 
-    it('second test', () => {
+//     it('second test', () => {
         
-    })
+//     })
 
-    it('third test', () => {
+//     it('third test', () => {
         
-    })
+//     })
 
-    it('forth test', () => {
+//     it('forth test', () => {
         
-    })
+//     })
 
-} )
+// } )
+
+/// <reference types="cypress"/>
 
 describe ( 'My Third test suite', () => {
 
     it('first test', () => {
-        
-    })
-
-    it('second test', () => {
-        
-    })
-
-    it('third test', () => {
-        
-    })
-
-    it('forth test', () => {
         
     })
 
